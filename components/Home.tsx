316e14d9411c1e5a1b0ecde5642430432fd137b1
Steph <stsn21@student.bth.se>
2022-04-19 19:59:13 +0200

    added route arg and pointed back to old style

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,9 +3,9 @@ import styles from './Styles';
 import sauceShelfImg from '../assets/sauceshelf.jpg';
 import Stock from './Stock';
 
-export default function Home({products, setProducts}) {
+export default function Home({route, products, setProducts}) {
     return <ScrollView style={styles.base}>
-        <Text style={styles.header}>Lager-Appen</Text>
+        <Text style={styles.heading}>Lager-Appen</Text>
         <Image source={sauceShelfImg} style={{ width: 320, height: 240, marginBottom: 28 }} />
         <Stock products={products} setProducts={setProducts} />
     </ScrollView>;
