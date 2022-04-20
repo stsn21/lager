55ee494b543ab7b31fa120addca84eb6f38e2b52
Steph <stsn21@student.bth.se>
2022-04-20 19:01:47 +0200

    added updateProduct

--- models/products.ts
+++ models/products.ts
@@ -1,7 +1,20 @@
 import config from '../config/config.json'
+import Product from '../interfaces/product'
 
 const products = {
-    // updateProduct: async function updateProduct() {}, //TODO
+    updateProduct: async function updateProduct(product: Partial<Product>): Promise<void> {
+        try {
+            await fetch(`${config.base_url}/products?api_key=${config.api_key}`, {
+                body: JSON.stringify(product),
+                headers: {
+                    'content-type': 'application/json'
+                },
+                method: 'PUT'
+            });
+        } catch (error) {
+            console.log("Could not update product");
+        };
+    },
     getProducts: async function getProducts() {
         const response = await fetch(`${config.base_url}/products?api_key=${config.api_key}`);
         const result = await response.json();
