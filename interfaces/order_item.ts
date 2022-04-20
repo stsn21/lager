fb0ecbba0d1b3ea8af5b9bbcdbf50faafdb699f3
Steph <stsn21@student.bth.se>
2022-04-20 18:59:21 +0200

    added interface

--- interfaces/order_item.ts
+++ interfaces/order_item.ts
@@ -1,1 +1,12 @@
-// export default interface OrderItem {}; // TODO
+export default interface OrderItem {
+    order_id: number,
+    product_id: number,
+    amount: number,
+    article_number: string,
+    name: string,
+    description: string,
+    specifiers: object,
+    stock: number,
+    location: string,
+    price: number,
+};
