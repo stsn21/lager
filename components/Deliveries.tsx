ede64080e9e37a98fbf4bb35bf12eccf92322231
Steph <stsn21@student.bth.se>
2022-05-04 18:13:50 +0200

    removed parentheses

--- components/Deliveries.tsx
+++ components/Deliveries.tsx
@@ -6,10 +6,8 @@ import DeliveryForm from './DeliveryForm';
 const Stack = createNativeStackNavigator();
 
 export default function Deliveries() {
-    return (
-        <Stack.Navigator initialRouteName="List">
+    return <Stack.Navigator initialRouteName="List">
             <Stack.Screen name="List" component={DeliveriesList} />
             <Stack.Screen name="Form" component={DeliveryForm} />
-        </Stack.Navigator>
-    );
+        </Stack.Navigator>;
 };
