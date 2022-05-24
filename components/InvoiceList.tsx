73ded798a1a85fa8867176b8bc45e1c3dcaee325
Steph <stsn21@student.bth.se>
2022-05-24 14:44:41 +0200

    getInvoices, invoice list

--- components/InvoiceList.tsx
+++ components/InvoiceList.tsx
@@ -1,17 +1,67 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import { ScrollView, Button, View, Text } from "react-native";
-import { Base } from "../styles/index";
+import { Base, Typography } from "../styles/index";
 
+import Invoice from '../interfaces/invoice';
+import invoicesModel from '../models/invoices';
 import authModel from '../models/auth';
 
 // TODO: list of invoices, reload invoices
 export default function InvoiceList({ route, navigation, setIsLoggedIn }) {
-    // useFocusEffect(
-    //     useCallback(() => {
-    //         reloadInvoices();
-    //     }, [])
-    // );
+    const [allInvoices, setAllInvoices] = useState([]);
+
+    useFocusEffect(
+        useCallback(() => {
+            reloadInvoices();
+        }, [])
+    );
+    
+    async function reloadInvoices() {
+        setAllInvoices(await invoicesModel.getInvoices());
+    };
+
+    // function sortByDateDescending (a: Partial<Delivery>, b: Partial<Delivery>): number {
+    //     if (a.delivery_date !== undefined && b.delivery_date !== undefined) {
+    //         if (a.delivery_date > b.delivery_date) {
+    //             return -1;
+    //         };
+    //         if (a.delivery_date < b.delivery_date) {
+    //             return 1;
+    //         };
+    //     };
+    //     return 0;
+    // }
+
+    let listOfInvoices: JSX.Element[] = [
+        <View key={-1} style={Base.multilineMenuContainer}>
+            <Text style={{
+                ...Typography.infoText,
+                textAlign: 'center'
+            }}>
+                No Invoices to display... 😰
+            </Text>
+        </View>
+    ];
+    if (allInvoices.length > 0) {
+        listOfInvoices = allInvoices
+        // .sort(sortByDateDescending)
+        .map((invoice: Partial<Invoice>, index: number) => {
+            return <View key={index} style={Base.multilineMenuContainer}>
+                <Text>Invoice ID: {invoice.id}</Text>
+                <Text>Order ID: {invoice.order_id}</Text>
+                <View style={{margin: 10}}>
+                    <Text>{invoice.name}</Text>
+                    <Text>{invoice.address}</Text>
+                    <Text>{invoice.zip} {invoice.city}</Text>
+                    <Text>{invoice.country}</Text>
+                </View>
+                <Text>Total price: {invoice.total_price}</Text>
+                <Text>Created: {invoice.creation_date}</Text>
+                <Text>Due: {invoice.due_date}</Text>
+            </View>;
+        });
+    };
 
     async function doLogout() {
         await authModel.logout();
@@ -20,9 +70,7 @@ export default function InvoiceList({ route, navigation, setIsLoggedIn }) {
     }
 
     return <ScrollView style={Base.base}>
-        <View style={Base.base}>
-            <Text>Placeholder!</Text>
-        </View>
+        {listOfInvoices}
         <Button
             title="New invoice"
             onPress={() => {
--- models/invoices.ts
+++ models/invoices.ts
@@ -1,1 +1,21 @@
 // TODO: functions to get and update invoices
+
+import config from "../config/config.json";
+import Invoice from "../interfaces/invoice";
+import storage from "../models/storage";
+
+const invoices = {
+    getInvoices: async function getInvoices(): Promise<Invoice[]> {
+        const token = await storage.readToken();
+        const response = await fetch(`${config.base_url}/invoices?api_key=${config.api_key}`, {
+            method: "GET",
+            headers: {
+                'x-access-token': token.token
+            }
+        });
+        const result = await response.json();
+        return result.data;
+    },
+};
+
+export default invoices;
