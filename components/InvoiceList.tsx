7496229245d57ddbe2e48b93275a3e695e676188
Steph <stsn21@student.bth.se>
2022-05-25 12:52:23 +0200

    display message when no invoices found

--- components/InvoiceList.tsx
+++ components/InvoiceList.tsx
@@ -33,36 +33,35 @@ export default function InvoiceList({ route, navigation, setIsLoggedIn }) {
     //     return 0;
     // }
 
-    // let listOfInvoices: JSX.Element[] = [
-    //     <View key={-1} style={Base.multilineMenuContainer}>
-    //         <Text style={{
-    //             ...Typography.infoText,
-    //             textAlign: 'center'
-    //         }}>
-    //             No Invoices to display... 😰
-    //         </Text>
-    //     </View>
-    // ];
-
-    // if (allInvoices.length > 0) {
-    const listOfInvoices = allInvoices
-    // .sort(sortByDateDescending)
-    .map((invoice: Partial<Invoice>, index: number) => {
-        return <View key={index} style={Base.multilineMenuContainer}>
-            <Text>Invoice ID: {invoice.id}</Text>
-            <Text>Order ID: {invoice.order_id}</Text>
-            <View style={{margin: 10}}>
-                <Text>{invoice.name}</Text>
-                <Text>{invoice.address}</Text>
-                <Text>{invoice.zip} {invoice.city}</Text>
-                <Text>{invoice.country}</Text>
-            </View>
-            <Text>Total price: {invoice.total_price}</Text>
-            <Text>Created: {invoice.creation_date}</Text>
-            <Text>Due: {invoice.due_date}</Text>
-        </View>;
-    });
-    // };
+    let listOfInvoices: JSX.Element[] = [
+        <View key={-1} style={Base.multilineMenuContainer}>
+            <Text style={{
+                ...Typography.infoText,
+                textAlign: 'center'
+            }}>
+                No Invoices to display... 😰
+            </Text>
+        </View>
+    ];
+    if (allInvoices.length > 0) {
+        listOfInvoices = allInvoices
+        // .sort(sortByDateDescending)
+        .map((invoice: Partial<Invoice>, index: number) => {
+            return <View key={index} style={Base.multilineMenuContainer}>
+                <Text>Invoice ID: {invoice.id}</Text>
+                <Text>Order ID: {invoice.order_id}</Text>
+                <View style={{margin: 10}}>
+                    <Text>{invoice.name}</Text>
+                    <Text>{invoice.address}</Text>
+                    <Text>{invoice.zip} {invoice.city}</Text>
+                    <Text>{invoice.country}</Text>
+                </View>
+                <Text>Total price: {invoice.total_price}</Text>
+                <Text>Created: {invoice.creation_date}</Text>
+                <Text>Due: {invoice.due_date}</Text>
+            </View>;
+        });
+    };
 
     async function doLogout() {
         await authModel.logout();
