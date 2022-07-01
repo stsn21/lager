5007a4a4026eef9b078ea453264237e48feba38a
Steph <stsn21@student.bth.se>
2022-07-01 11:57:58 +0200

    kopierad kod från övning GPS och karta

--- components/ShipOrder.tsx
+++ components/ShipOrder.tsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
 import { Base, Typography, Tables } from "../styles/index";
 import { DataTable } from "react-native-paper";
 
+import MapView from 'react-native-maps';
+import { Marker } from "react-native-maps";
+import * as Location from 'expo-location';
+
 import orderModel from "../models/orders";
+import getCoordinates from "../models/nominatim";
 
 export default function ShipOrder({ route, navigation }) {
     const { order } = route.params;
+    const [marker, setMarker] = useState(null);
+    const [locationMarker, setLocationMarker] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const results = await getCoordinates(`${order.address}, ${order.city}`);
+
+            setMarker(<Marker
+                coordinate={{ latitude: parseFloat(results[0].lat), longitude: parseFloat(results[0].lon) }}
+                title={results[0].display_name}
+            />);
+        })();
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+    
+            if (status !== 'granted') {
+                setErrorMessage('Permission to access location was denied');
+                return;
+            };
+    
+            const currentLocation = await Location.getCurrentPositionAsync({});
+    
+            setLocationMarker(<Marker
+                coordinate={{
+                    latitude: currentLocation.coords.latitude,
+                    longitude: currentLocation.coords.longitude
+                }}
+                title="Min plats"
+                pinColor="blue"
+            />);
+        })();
+    }, []);
 
     async function doShip() {
         await orderModel.shipOrder(order);
@@ -48,6 +90,18 @@ export default function ShipOrder({ route, navigation }) {
             </DataTable.Row>
         </DataTable>
 
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 56.1612,
+                longitude: 15.5869,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
+            }}>
+            {marker}
+            {locationMarker}
+        </MapView>
+
         <Text style={ Typography.label }>Items</Text>
         <DataTable>
             <DataTable.Header>
@@ -60,3 +114,14 @@ export default function ShipOrder({ route, navigation }) {
         <Button title="Ship this order" color={Base.accentColor} onPress={doShip}/>
     </View>;
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
